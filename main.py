a22fffb8a14bee558a43d77c60a6a417d6f1a85e
xOwenxz <rodrigopereirarp7@gmail.com>
2024-03-30 13:41:03 -0300

    01

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Projeto Curso Python
+Docstrings para informação
+
+"""
+
+''' 
+Explicar pedaços de código
+'''
+
+# Olá mundo
+print('Hello World!')
+
+# Permite escrever um comentário
+print(123) # na frente
+# abaixo
+print(2 + 1)
